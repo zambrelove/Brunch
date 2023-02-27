e5d694717d77d27a8b876bdb44668da981f0537f
Love Zambre <zambrelove95@gmail.com>
2023-02-27 11:14:00 +0530

    add some Asset and section on WebPage

---
+++ Assets/index.js
@@ -0,0 +1,11 @@
+//Open Sidebar
+function OpenSidebar(){
+    var element = document.getElementById("Sidebar");
+    element.classList.add("active");
+}
+
+//Close Sidebar
+function CloseSidebar(){
+    var element = document.getElementById("Sidebar");
+    element.classList.remove("active");
+}
